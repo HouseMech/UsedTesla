1dd047ad01e9460e21c22a1da05fd41244345636
Liam Harbec <liam.harbec@gmail.com>
2020-05-23 11:19:34 -0700

    Updated the graph so that it can properly display data from Car and CarDatum models but only reads the first Car in the database as a test

--- app/javascript/graph.js
+++ app/javascript/graph.js
@@ -1,24 +1,29 @@
 import Chart from 'chart.js';
 
-$(document).on('turbolinks:load', function() {
-
-  var ctx = document.getElementById('myChart').getContext('2d');
+function graphIt() {
+  let prices = car_data.map(item => item.car_price);
+  let labels = car_data.map(item => item.date_acquired)
+  var ctx = document.getElementById('myChart')
   var chart = new Chart(ctx, {
-      // The type of chart we want to create
-      type: 'line',
+    // The type of chart we want to create
+    type: 'line',
 
-      // The data for our dataset
-      data: {
-          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-          datasets: [{
-              label: 'My First dataset',
-              backgroundColor: 'rgb(255, 99, 132)',
-              borderColor: 'rgb(255, 99, 132)',
-              data: [0, 10, 5, 2, 20, 30, 45]
-          }]
-      },
+    // The data for our dataset
+    data: {
+        labels: labels,
+        datasets: [{
+            label: 'Car prices',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: prices
+        }]
+    },
 
-      // Configuration options go here
-      options: {}
+    // Configuration options go here
+    options: {}
   });
+}
+
+$(document).on('turbolinks:load', function() {
+  graphIt();
 });
