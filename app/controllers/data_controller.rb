51684421cf9e6d4d560e3d501723f35ddd69c7b4
Liam Harbec <liam.harbec@gmail.com>
2020-05-25 13:28:31 -0700

    Added Vehicle controller

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -1,7 +1,4 @@
 class DataController < ApplicationController
-  #The index page grab the price data from each of the used Tesla models and insert it into a graph.
   def index
-    @current_car = Car.first
-    @current_car_data = @current_car.car_data.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'data/index'
   root 'data#index'
+
+  resources :vehicles, only: [:index, :show]
 end
