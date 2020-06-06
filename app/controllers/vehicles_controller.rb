29a847482ec089da620f306a5c8d191071f2f5d2
Liam Harbec <liam.harbec@gmail.com>
2020-06-06 10:20:02 -0700

    Added an option to view sold vehicles

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -3,6 +3,10 @@ class VehiclesController < ApplicationController
     @vehicles = Vehicle.all.where(sold: false)
   end
 
+  def sold
+    @vehicles = Vehicle.all.where(sold: true)
+  end
+
   def show
     @current_vehicle = Vehicle.find_by(vin: params[:id])
     @current_vehicle_data = @current_vehicle.vehicle_data.all
